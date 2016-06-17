a819f905f215af4ccb20432e028a16de0208bfeb
Luke Ulrich <lukeulrich@users.noreply.github.com>
2016-06-17 08:00:33 -0400

    Update: added documentation, default middlewares, unit tests

--- test-data/routing/^patch.js
+++ test-data/routing/^patch.js
@@ -1,6 +1,5 @@
 'use strict'
 
 module.exports = function(app, middlewares, routeMiddlewares) {
-	console.log('swine')
 	return middlewares.validToken
 }
--- test-data/routing/skills/$type/patch.js
+++ test-data/routing/skills/$type/patch.js
@@ -2,7 +2,7 @@
 
 module.exports = function(app, middlewares, routeMiddlewares) {
 	return function(req, res, next) {
-		res.locals.stack.push('get /caret-without-star')
+		res.locals.stack.push(`patch /skills/${req.params.type}`)
 		res.send(res.locals.stack)
 	}
 }
